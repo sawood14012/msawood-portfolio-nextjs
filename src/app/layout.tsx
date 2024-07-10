dff0323a30b3687a87673714f746a6d6b9b2a907
Meer Sawood <msawood@redhat.com>
2024-07-10 12:03:31 +0530

    added tag manager stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><script src="/tag.js" /></head>
       <body className={inter.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THDJ56GX"
+          height="0" width="0" className="display:none;visibility:hidden"></iframe></noscript>
         {children}
       </body>
     </html>
