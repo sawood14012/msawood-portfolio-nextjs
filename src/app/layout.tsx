bbe474f57df0e78356f8818005f60eea9fac3ead
Meer Sawood <msawood@redhat.com>
2024-07-10 12:13:04 +0530

    use next/script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><script src="/tag.js" /></head>
+      <head>
+        <Script strategy="beforeInteractive" src="/tag.js" />
+        <Script strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-YCWGZB20CY"></Script>
+        <Script strategy="beforeInteractive" src="/analytics.js" />
+      </head>
       <body className={inter.className}>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THDJ56GX"
           height="0" width="0" className="display:none;visibility:hidden"></iframe></noscript>
